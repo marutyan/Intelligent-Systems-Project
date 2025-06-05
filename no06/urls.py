e9f80a04b0222816bc64f20632071e2812499e88
marutyan <132348090+marutyan@users.noreply.github.com>
2025-06-06 01:03:16 +0900

    chore(router): unify all feature endpoints

--- no06/urls.py
+++ no06/urls.py
@@ -4,18 +4,20 @@ from . import views
 urlpatterns = [
     path('',                   views.index,             name='index'),
     path('upload/',            views.upload_video,      name='upload_video'),
+
+    # ① データ生成 / 分布
     path('selectVideos/',      views.select_videos,     name='select_videos'),
     path('generateData/',      views.generate_data,     name='generate_data'),
     path('make_distribution/', views.make_distribution, name='make_distribution'),
     path('showDistribution/',  views.show_distribution, name='show_distribution'),
+
+    # ② ファイル・画像・サムネ
+    path('showDirectory/',     views.show_directory,    name='show_directory'),
     path('getImage/',          views.get_image,         name='get_image'),
-    path('getVideo/',          views.get_video,         name='get_video'),
     path('getThumbList/',      views.get_thumb_list,    name='get_thumb_list'),
 
-    # --- BBox detection ---
+    # ③ BBox 検出
     path('selectDetect/',      views.select_detect,     name='select_detect'),
     path('bboxDetect/',        views.bbox_detect,       name='bbox_detect'),
-    path('getDetVideo/',      views.get_det_video,     name='get_det_video'),
-
-    path('showDirectory/',     views.show_directory,    name='show_directory'),
+    path('getDetVideo/',       views.get_det_video,     name='get_det_video'),
 ]
